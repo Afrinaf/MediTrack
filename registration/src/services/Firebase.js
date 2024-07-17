62bbae94b0b737b36471e1a78ce9a08e2eddfa1a
Afrinaf <107719683+Afrinaf@users.noreply.github.com>
2024-07-17 19:10:03 +0530

    Update Firebase.js

--- registration/src/services/Firebase.js
+++ registration/src/services/Firebase.js
@@ -4,13 +4,13 @@ import { getDatabase } from 'firebase/database';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBUj2mbmLXiIpii7KIQDvWbSnDNFg2JjWU",
-    authDomain: "eprescription-7a141.firebaseapp.com",
-    projectId: "eprescription-7a141",
-    storageBucket: "eprescription-7a141.appspot.com",
-    messagingSenderId: "116865623448",
-    appId: "1:116865623448:web:6cb5e93a2b8828af5ab7ed",
-    measurementId: "G-3GWZM19068"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase
